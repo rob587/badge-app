bf187596324964ff50ee50627bbd586d03eff429
rob587 <robertocamm02@yahoo.com>
2026-03-27 11:32:14 +0100

    Refactor Index component to display user information with styled layout

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,52 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <>
+      <View style={styles.contenitore}>
+        <View style={styles.box}>
+          <View style={styles.boxName}>
+            <Text>RC</Text>
+          </View>
+          <Text style={styles.mainNome}>Roberto Cammarata</Text>
+          <Text style={styles.subName}>Junior Frontend Developer</Text>
+        </View>
+      </View>
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  contenitore: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  box: {
+    borderRadius: 15,
+    height: 200,
+    backgroundColor: "#4f46e5",
+    justifyContent: "center",
+    alignItems: "center",
+    width: 200,
+  },
+
+  boxName: {
+    borderRadius: 15,
+    borderStyle: "solid",
+    borderColor: "#818cf8",
+    height: 40,
+  },
+
+  mainNome: {
+    textAlign: "center",
+    fontSize: 25,
+    color: "white",
+  },
+
+  subName: {
+    color: "#9bb4fc",
+    margin: 5,
+  },
+});
